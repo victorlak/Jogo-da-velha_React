09b4fb3cc5d1f7dd27988afe2c3b6b1175f4bfd8
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2024-07-02 20:49:14 -0300

    Minha versao do jogo da velha

--- src/App.js
+++ src/App.js
@@ -1,144 +1,114 @@
 import {React, useState, useEffect} from 'react';
-import { Button, Col, Row } from 'reactstrap';
+import { Button, Col, Container, Row } from 'reactstrap';
 import './Style/App.css'
 
 function App() {
+  const [jogador, setJogador] = useState('X')
+  const [jogadorX, setJogadorX] = useState([])
+  const [jogadorO, setJogadorO] = useState([])
 
-  function zerarTabuleiro(){
-    setOpcao1('')
-    setOpcao2('')
-    setOpcao3('')
-    setOpcao4('')
-    setOpcao5('')
-    setOpcao6('')
-    setOpcao7('')
-    setOpcao8('')
-    setOpcao9('')
-  }
-  const[jogador, setJogador] = useState('x')
-  const [opcao1, setOpcao1] = useState('')
-  const [opcao2, setOpcao2] = useState('')
-  const [opcao3, setOpcao3] = useState('')
+  const [close, setClose] = useState(false)
 
-  const [opcao4, setOpcao4] = useState('')
-  const [opcao5, setOpcao5] = useState('')
-  const [opcao6, setOpcao6] = useState('')
-
-  const [opcao7, setOpcao7] = useState('')
-  const [opcao8, setOpcao8] = useState('')
-  const [opcao9, setOpcao9] = useState('')
-  
-  useEffect(() => {
-    console.log(jogador); 
-    vitorias.map((element)=>{
-      if(!(element.includes(''))){
-        if(element.every((element,index,array)=>element===array[0])===true){
-          zerarTabuleiro()
-          setJogador('x')
-        }
-     }
-    })
-  }, [opcao1, opcao2, opcao3, opcao4, opcao5, opcao6, opcao7, opcao8, opcao9,]);
-  const vitorias = [
-    [opcao1,opcao2,opcao3],
-    [opcao4,opcao5,opcao6],
-    [opcao7,opcao8,opcao9],
-    [opcao1,opcao5,opcao9],
-    [opcao3,opcao5,opcao7],
-    [opcao1,opcao4,opcao7],
-    [opcao2,opcao5,opcao8],
-    [opcao3,opcao6,opcao9]
+  const sequenciaVitoria = [
+    [1,2,3], [4,5,6], [7,8,9],
+    [1,4,7], [2,5,8], [3,6,9],
+    [1,5,9], [3,5,7]
   ]
+
+  function trocaVez(){
+    if(jogador === 'X'){
+      setJogador('O')
+    }else{
+      setJogador('X')
+    }
+  }
+
+  function verificaVitoria(movimentos) {
+    return sequenciaVitoria.some(sequencia =>
+      sequencia.every(numero => movimentos.includes(numero))
+    );
+  }
+
+  useEffect(()=> {
+
+    if(verificaVitoria(jogadorX)){
+      alert('Jogador X ganhou')
+
+      setClose(true)
+
+    }else if(verificaVitoria(jogadorO)){
+      alert('Jogador O ganhou')
+
+      setClose(true)
+    }
+
+    if (jogadorX.length + jogadorO.length === 9 && !close) {
+      alert('Deu velha')
+      setClose(true)
+    }
+
+  }, [jogador])
+
+  function handleClick(event){
+    const casa = Number(event.target.id)
+    if (close) {
+      alert('Jogo finalizado')
+      return
+    }
+    //exibir o x ou o o na tela
+    const elemento = document.getElementById(casa)
+    elemento.innerHTML = `<span class=jogada> ${jogador}</span>`
+    elemento.style.pointerEvents = 'none'
+
+    if (jogador === 'X' ) {
+      setJogadorX([...jogadorX, casa])
+    }else{
+      setJogadorO([...jogadorO, casa])
+    }
+
+    trocaVez()
+  }
+  /*
+  Linhas horizontais:
+      [1, 2, 3]
+      [4, 5, 6]
+      [7, 8, 9]
+
+      Linhas verticais:
+      [1, 4, 7]
+      [2, 5, 8]
+      [3, 6, 9]
+
+      Diagonais:
+      [1, 5, 9]
+      [3, 5, 7]
+  */
+
   return (
-    <div className="All">
-        <Row className='Tabuleiro'>
-          <Col style={{backgroundColor:'red'}} onClick={()=>{if(opcao1 === ''){
-            if(jogador === 'x'){
-              setOpcao1('x')
-              setJogador('o')
-            }else{
-              setOpcao1('o')
-              setJogador('x')
-            }
-          }
-          }} md={4}className='Opcao'><h1>{opcao1}</h1></Col>
-          <Col style={{backgroundColor:'red'}} onClick={()=>{if(opcao2 === ''){
-            if(jogador === 'x'){
-              setOpcao2('x')
-              setJogador('o')
-            }else{
-              setOpcao2('o')
-              setJogador('x')
-            }
-          }
-          }}md={4}className='Opcao CentroSuperior'><h1>{opcao2}</h1></Col>
-          <Col style={{backgroundColor:'red'}} onClick={()=>{if(opcao3 == ''){
-            if(jogador === 'x'){
-              setOpcao3('x')
-              setJogador('o')
-            }else{
-              setOpcao3('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao'><h1>{opcao3}</h1></Col>
+    <>
+    <div className="Placar mt-1">
+      {
+        close && <Button color="success" onClick={()=> window.location.reload()}>Reiniciar</Button>
+      }
 
-          <Col style={{backgroundColor:'red'}} onClick={()=>{if(opcao4 == ''){
-            if(jogador === 'x'){
-              setOpcao4('x')
-              setJogador('o')
-            }else{
-              setOpcao4('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao PrimeiroMeio'><h1>{opcao4}</h1></Col>
-          <Col onClick={()=>{if(opcao5 == ''){
-            if(jogador === 'x'){
-              setOpcao5('x')
-              setJogador('o')
-            }else{
-              setOpcao5('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao CentroMeio'><h1>{opcao5}</h1></Col>
-          <Col onClick={()=>{if(opcao6 == ''){
-            if(jogador === 'x'){
-              setOpcao6('x')
-              setJogador('o')
-            }else{
-              setOpcao6('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao TerceiroMeio'><h1>{opcao6}</h1></Col>
-
-          <Col onClick={()=>{if(opcao7 == ''){
-            if(jogador === 'x'){
-              setOpcao7('x')
-              setJogador('o')
-            }else{
-              setOpcao7('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao'><h1>{opcao7}</h1></Col>
-          <Col onClick={()=>{if(opcao8 == ''){
-            if(jogador === 'x'){
-              setOpcao8('x')
-              setJogador('o')
-            }else{
-              setOpcao8('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao CentroInferior'><h1>{opcao8}</h1></Col>
-          <Col onClick={()=>{if(opcao9 == ''){
-            if(jogador === 'x'){
-              setOpcao9('x')
-              setJogador('o')
-            }else{
-              setOpcao9('o')
-              setJogador('x')
-            }
-          }}}md={4}className='Opcao'><h1>{opcao9}</h1></Col>
-        </Row>
     </div>
+    <div className="All">
+    <div className="Tabuleiro">
+        <div onClick={handleClick} id='1' className="box A1"></div>
+        <div onClick={handleClick} id='2' className="box A2"></div>
+        <div onClick={handleClick} id='3' className="box A3"></div>
+        <div onClick={handleClick} id='4' className="box B1"></div>
+        <div onClick={handleClick} id='5' className="box B2"></div>
+        <div onClick={handleClick} id='6' className="box B3"></div>
+        <div onClick={handleClick} id='7' className="box C1"></div>
+        <div onClick={handleClick} id='8' className="box C2"></div>
+        <div onClick={handleClick} id='9' className="box C3"></div>
+    </div>
+
+</div>
+
+
+</>
   );
 }
 
